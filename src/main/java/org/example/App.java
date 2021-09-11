8684970aacdf622fe2cccf2deae30622e0de5009
DynieM <dynieme.fms@gmail.com>
2021-09-10 23:00:34 -0400

    Update

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.util.Scanner;
+/**
+ * UCF COP3330 Fall 2021 Assignment 1 Solution
+ * Copyright 2021 Dynie Mesoneuvre
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print( "What is the quote?" );
+        String quote = input.nextLine(); //Input
+        System.out.print("Who said it?");
+        String person = input.nextLine();
+        String concat = (person + " says " + "\"" + quote + ".\"" );
+        System.out.println(concat); //Output
+
+
+    }
+}
